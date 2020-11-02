1f8023cc851fcb5a14cb2a2a95146d3ced23ee0b
Pavel Svec <pavel.svec@itcrawl.cz>
2020-11-02 11:46:37 +0100

    Add project files.

---
+++ WpfApp1/Acc.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WpfApp1
+{
+    public delegate void OnChangedBalance();
+    public abstract class BankAccount
+    {
+        public event OnChangedBalance ChangedBalance;
+
+        internal decimal balance { get; set; }
+        internal decimal interest { get; set; } = 0.005m;
+        //
+
+        public void DefaultChangedBalance()
+        {
+            Debug.WriteLine("Default Changed Balance");
+        }
+
+        public void Subscribe(OnChangedBalance f)
+        {
+            ChangedBalance += f;
+        }
+        public BankAccount(decimal startBalance = 10_000)
+        {
+            Subscribe(DefaultChangedBalance);
+
+            balance = startBalance;
+            ChangedBalance();
+        }
+        public abstract void MonthlyInterest();
+        public string Balance()
+        {
+            return $"Balance: {balance:#.##} Kč";
+        }
+        public void Withdraw(int amount)
+        {
+            balance -= amount;
+            ChangedBalance();
+        }
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            ChangedBalance();
+        }
+        public virtual bool CheckWithdraw(decimal amount)
+        {
+            if (balance - amount > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+    public class SavingsAccount : BankAccount
+    {
+        public SavingsAccount(decimal startBalance) : base(startBalance)
+        {
+            interest = 0.01m;
+        }
+        public override void MonthlyInterest()
+        {
+            decimal monthlyInterest = balance * interest;
+            balance += monthlyInterest;
+        }
+    }
+    public class StudentSavingsAccount : SavingsAccount
+    {
+        public StudentSavingsAccount(decimal startBalance) : base(startBalance)
+        {
+
+        }
+        public override void MonthlyInterest()
+        {
+            decimal monthlyInterest = balance * interest;
+            balance += monthlyInterest;
+        }
+    }
+    public class CreditAccount : BankAccount
+    {
+        public CreditAccount(decimal startBalance) : base(startBalance)
+        {
+
+        }
+        int negative = 5_000;
+        decimal baseTax = 15;
+        public override void MonthlyInterest()
+        {
+            decimal interestAndTax = 0;
+            if (balance > 0)
+            {
+                interestAndTax += balance * interest;
+                interestAndTax += baseTax;
+            }
+            else if (balance > -negative)
+            {
+                // rate of pain is going to decline
+                interestAndTax += (negative - balance) * interest;
+                interestAndTax += baseTax;
+            }
+            else
+            {
+                balance = negative; //interestAndTag still gon be 0
+            }
+            balance -= interestAndTax;
+        }
+        public override bool CheckWithdraw(decimal amount)
+        {
+            if (balance + negative - amount > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SavingsAccount sa;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sa = new SavingsAccount(0.0m);
+            sa.Subscribe(UpdateBalanceLabel);
+            sa.Subscribe(UpdateDebugInfo);
+        }
+
+        private void UpdateBalanceLabel()
+        {
+            NewBalance.Content = sa.balance.ToString();
+        }
+
+        private void UpdateDebugInfo()
+        {
+            Debug.WriteLine($"New balance: {sa.balance}");
+        }
+
+        private void Deposit_Click(object sender, RoutedEventArgs e)
+        {
+            sa.Deposit(int.Parse(Amount.Text));
+        }
+    }
+}
